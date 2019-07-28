cc75dc665204c4b7fd636a0c3143a14dd4c163a3
unknown <himanshudhiman9313@gmail.com>
2019-07-28 15:12:35 +0530

    Problems table created

--- Threshold/models.py
+++ Threshold/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Problem(models.Model):
+    difficulty_level = [
+        ('e', 'easy'),
+        ('m', 'medium'),
+        ('h', 'hard')
+    ]
+    part = [
+        ('ll', 'Linked List'),
+        ('q', 'Queue')
+    ]
+    name = models.CharField(max_length = 50)
+    description = models.TextField()
+    image = models.ImageField()
+    level = models.CharField(max_length = 10, choices = difficulty_level)
+    topic = model.CharField(max_length = 100, choices = part)
+
+    def __str__(self):
+        return self.name
