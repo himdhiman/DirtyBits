6e1759f2a09fb9626c5d2054475605883c740122
Gaurav Verma <gaurav.gv412@gmail.com>
2019-07-30 00:53:02 +0530

    made compulsary to enter some fields on signup

--- DirtyBits/settings.py
+++ DirtyBits/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dirtybits',
-        'USER': 'postgres',
-        'PASSWORD': 'Himanshu9313@',
+        'USER': 'gauravverma',
+        'PASSWORD': 'gauravverma',
         'HOST': 'localhost'
     }
 }
--- auth/views.py
+++ auth/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 # Create your views here.
 
+class Dirtyexception(Exception):
+    def __init__(self, error_list):
+        self.error_list = error_list
 
 def SignUp(request):
     error = ""
@@ -13,9 +16,26 @@ def SignUp(request):
         username = request.POST['username']
         password = request.POST['password']
         email = request.POST['email']
+        e1 = e2 = e3 = ''
         try:
+            if username == '':
+                e1 = 'Username is necessory'
+            if email == '':
+                e2 ='Email is necessory'
+            if password == '':
+                e3 ='Password is necessory'
+            if e1 != '' or e2 != '' or e3 != '':
+                error = [e1, e2, e3]
+                raise Dirtyexception(error)
             User.objects.get(username = username)
-        except:
+        except Dirtyexception as err:
+            context = {
+                "error1": err.error_list[0],
+                "error2": err.error_list[1],
+                "error3": err.error_list[2]
+            }
+            return render(request, 'auth/signup.html', context)
+        except :
             user = User.objects.create_user(username, email, password)
             user.first_name = request.POST['first_name']
             user.last_name = request.POST['last_name']
@@ -23,7 +43,7 @@ def SignUp(request):
             return render(request, 'auth/afterSignup.html')
         error = "Username already exists"
         context = {
-            error: "error"
+            "error": error
         }
         return render(request, 'auth/signup.html', context)  
     else:
